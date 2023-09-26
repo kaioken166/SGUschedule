94a6ca57920c9f670e69a3757d4e0c622a282dc1
Tri <trihuynh02012002@gmail.com>
2023-09-26 10:42:23 +0700

    update render data

---
+++ js/functionalScript.js
@@ -0,0 +1,132 @@
+// window.onload = function () {
+
+// }
+
+var storedSubjects = JSON.parse(localStorage.getItem('danhsachmonHoc'));
+
+function renderTable() {
+    var tableHead =
+        `<thead>
+            <tr class="table-primary">
+                <th class="border-end px-4">Thứ/Giờ</th>
+                <th class="border-end px-4">Thứ hai</th>
+                <th class="border-end px-4">Thứ ba</th>
+                <th class="border-end px-4">Thứ tư</th>
+                <th class="border-end px-4">Thứ năm</th>
+                <th class="border-end px-4">Thứ sáu</th>
+                <th class="border-end px-4">Thứ bảy</th>
+                <th class="border-end px-4">Chủ nhật</th>
+            </tr>
+        </thead>`;
+
+    var string = 
+    `<tbody>
+        <tr>
+            <td class="border-end align-middle bg-light px-4">7h</td>
+            <td class="border-end px-4">T2</td>
+            <td class="border-end px-4">Row 2</td>
+            <td class="border-end px-4">Row 2</td>
+            <td class="border-end px-4">Row 2</td>
+            <td class="border-end px-4">Row 2</td>
+            <td class="border-end px-4">Row 2</td>
+            <td class="border-end px-4"></td>
+        </tr>
+        <tr>
+            <td class="border-end align-middle bg-light px-4">7h50</td>
+            <td class="border-end px-4">T2</td>
+            <td class="border-end px-4">Row 2</td>
+            <td class="border-end px-4">Row 2</td>
+            <td class="border-end px-4">Row 2</td>
+            <td class="border-end px-4">Row 2</td>
+            <td class="border-end px-4">Row 2</td>
+            <td class="border-end px-4"></td>
+        </tr>
+        <tr>
+            <td class="border-end align-middle bg-light px-4">9h</td>
+            <td class="border-end px-4">T2</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td id="insertHere" class="border-end px-4">Row 3</td>
+            <!-- <td class="border-end px-4" rowspan="3">Row 3</td> -->
+            <td class="border-end px-4"></td>
+        </tr>
+        <tr>
+            <td class="border-end align-middle bg-light px-4">9h50</td>
+            <td class="border-end px-4">T2</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <!-- <td class="border-end px-4">Row 3</td> -->
+            <td class="border-end px-4"></td>
+        </tr>
+        <tr>
+            <td class="border-end align-middle bg-light px-4">10h40</td>
+            <td class="border-end px-4">T2</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <!-- <td class="border-end px-4">Row 3</td> -->
+            <td class="border-end px-4"></td>
+        </tr>
+        <tr>
+            <td colspan="8" class="text-center align-middle">Nghỉ trưa</td>
+        </tr>
+        <tr>
+            <td class="border-end align-middle bg-light px-4">1h</td>
+            <td class="border-end px-4">T2</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4"></td>
+        </tr>
+        <tr>
+            <td class="border-end align-middle bg-light px-4">1h50</td>
+            <td class="border-end px-4">T2</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4"></td>
+        </tr>
+        <tr>
+            <td class="border-end align-middle bg-light px-4">3h</td>
+            <td class="border-end px-4">T2</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4"></td>
+        </tr>
+        <tr>
+            <td class="border-end align-middle bg-light px-4">3h50</td>
+            <td class="border-end px-4">T2</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4"></td>
+        </tr>
+        <tr>
+            <td class="border-end align-middle bg-light px-4">4h40</td>
+            <td class="border-end px-4">T2</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4">Row 3</td>
+            <td class="border-end px-4"></td>
+        </tr>
+    </tbody>`;
+
+    var tableBody = `<tbody>` + string + `</tbody>`;
+
+    document.getElementById("scheduletable").innerHTML = tableHead + tableBody;
+}
